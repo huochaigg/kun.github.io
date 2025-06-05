6e51fa31469807281862413b9dcce069dee116a9
dk <1272664675@qq.com>
2025-06-05 10:43:05 +0800

    init vitepress blog

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+  title: "我的博客",
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "关于我", link: "/about" },
+    ],
+    sidebar: [
+      {
+        text: "Obsidian",
+        items: [
+          { text: "快捷键", link: "/obsidian/快捷键" },
+        ],
+      },
+    ],
+  },
+}
+)
