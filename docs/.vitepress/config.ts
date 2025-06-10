3042f6adc0d537fe6054eb00a058e0a79b2c99a2
dk <1272664675@qq.com>
2025-06-10 15:44:42 +0800

    BFC，IFC

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,16 +7,50 @@ export default defineConfig({
   themeConfig: {
     nav: [
       { text: "首页", link: "/" },
+      { text: "前端学习", link: "/web" },
       { text: "关于我", link: "/about" },
     ],
-    sidebar: [
-      {
-        text: "javascript",
-        items: [
-          { text: "快捷键", link: "/obsidian/快捷键" },
-        ],
-      },
-    ],
+    sidebar: {
+      "/web/": [
+        {
+          text: "javascript",
+          items: [
+            { text: "快捷键", link: "/obsidian/快捷键" },
+          ],
+        },
+        {
+          text: "css",
+          items: [
+            { text: "BFC", link: "/css/BFC" },
+            { text: "IFC", link: "/css/IFC" },
+          ],
+        },
+        {
+          text: "react",
+          items: [
+            { text: "setState同步异步", link: "/react/setState同步异步" },
+          ],
+        },
+        {
+          text: "vue",
+          items: [
+            
+          ],
+        },
+        {
+          text: "typescript",
+          items: [
+            
+          ],
+        },
+        {
+          text: "其他文件夹",
+          items: [
+            { text: "文件名", link: "/文件夹/文件名" },
+          ],
+        },
+      ]
+    },
   },
 }
 )
